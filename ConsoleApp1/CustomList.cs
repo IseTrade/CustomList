3b9c0ed906a1bbb51ed1fd22f2ddde98d950ca08
IseTrade <IseTrade@gmail.com>
2018-10-04 10:53:16 -0500

    first commit

---
+++ ConsoleApp1/CustomList.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class CustomList<T>
+    {
+        string value;
+        T[] items = new T[2];
+        int count;
+
+        // Define the indexer to allow client code to use [] notation.
+        public T this[int i]
+        {
+            get { return items[i]; }
+            set { items[i] = value; }
+        }
+        public void Add(T value)
+        {
+
+            //items[1] = 
+            items[0] = value;
+            //for (int i = 0; )
+
+            //this.items[1] = value;
+        }
+    }
+}
---
+++ CustomListUnitTestProject/UnitTest1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConsoleApp1;
+namespace CustomListUnitTestProject
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()   //Testing to see if value passed to customList.Add is the same as value 
+                                    //stored at index 0 of customList.
+        {
+            //Arrange
+            CustomList<int> customList = new CustomList<int>();
+            int value = 16;
+
+            //Act         
+            customList.Add(value);
+
+            //Assert
+           
+            Assert.AreEqual(value, customList[0]);
+        }
+
+        [TestMethod]
+        public void TestMethod2()  //Testing to see if an actual integer is passed and stored correctly. 
+        {
+            //Arrange
+            CustomList<int> customList = new CustomList<int>();
+            //int value = 16;
+
+            //Act
+            customList.Add(16);
+
+            //Assert
+
+            Assert.AreEqual(16, customList[0]);
+        }
+
+        [TestMethod]
+        public void TestMethod3()  //Test passing a negative int.
+        {
+            //Arrange
+            CustomList<int> customList = new CustomList<int>();
+           
+            //Act
+            customList.Add(-16);
+
+            //Assert
+
+            Assert.AreEqual(-16, customList[0]);
+        }
+
+        [TestMethod]
+        public void TestMethod4()  //Test passing a string
+        {
+            //Arrange
+            CustomList<string> customList = new CustomList<string>();
+            string value = "Hello World!!!";
+
+            //Act
+            customList.Add(value);
+
+            //Assert
+
+            Assert.AreEqual(value, customList[0]);
+        }
+
+        [TestMethod]
+        public void TestMethod5()  //Test passing null
+        {
+            //Arrange
+            CustomList<string> customList = new CustomList<string>();
+            string value = "";
+
+            //Act
+            customList.Add(value);
+
+            //Assert
+
+            Assert.AreEqual(value, customList[0]);
+        }
+
+
+        //[TestMethod]
+        //public void TestMethod2()
+        //{
+        //    //Arrange
+        //    CustomList<int> customList = new CustomList<int>();
+        //    int value = 16;
+        //    int countValue = 1;
+
+        //    //Act
+        //    customList.Add(value);
+
+        //    //Assert
+        //    Assert.AreEqual(customList.Count, countValue);
+        //}
+        [TestMethod]
+        public void TestMethod9()
+        {
+            //Arrange
+            CustomList<int> customList = new CustomList<int>();
+            int value = 16;
+            int value1 = 20;
+
+
+            //Act
+            customList.Add(value);
+            customList.Add(value1);
+
+            //Assert
+            Assert.AreEqual(value, customList[0]);
+        }
+    }
+}
